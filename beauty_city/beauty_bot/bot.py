ee55e1c41e5e3dda08c27e870dbc8e1f96ab18ad
Amirzhan Albariyev <albariev123@gmail.com>
2024-12-18 19:01:14 +0300

    Add functions

--- beauty_city/beauty_bot/bot.py
+++ beauty_city/beauty_bot/bot.py
@@ -1,290 +1,149 @@
-import telebot
-from telebot.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove, InlineKeyboardButton, InlineKeyboardMarkup
+import os
+import django
+from telebot import TeleBot
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardRemove
+from django.conf import settings
 
+# Убедитесь, что Django настроен
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'beauty_city.settings')  # Укажите путь к вашему settings.py
+django.setup()
 
-bot = telebot.TeleBot("7686380954:AAFyuxX2Vx7diqpF-bM5HV133z5rGe214sA")
+from beauty_bot.models import Services, Specialist, Clients, TimeSlot, Schedule  # Импорт моделей
 
+bot = TeleBot("7686380954:AAFyuxX2Vx7diqpF-bM5HV133z5rGe214sA")  # Замените на ваш API ключ
 
 user_states = {}
 
-
 @bot.message_handler(commands=['start'])
 def handle_start(message):
-    # Тут просят соглашение
-
     user_states[message.chat.id] = 'start'
-    # менюшка соглашения
-    keyboard = ReplyKeyboardMarkup(one_time_keyboard=True)
+    keyboard = ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)  # уменьшены кнопки
     button1 = KeyboardButton('да')
     button2 = KeyboardButton('нет')
     keyboard.add(button1, button2)
 
-    bot.reply_to(message, 'Привет! Я бот. Ты принимаешь соглашение?',
-                 reply_markup=keyboard)
+    bot.reply_to(message, 'Привет! Ты принимаешь соглашение?', reply_markup=keyboard)
 
-
-@bot.message_handler(func=lambda message: user_states.get(
-        message.chat.id) == 'start')
+@bot.message_handler(func=lambda message: user_states.get(message.chat.id) == 'start')
 def handle_accept(message):
-    # обработчик соглашения
-
-    # удалятся клавиатура
     remove_keyboard = ReplyKeyboardRemove()
-
-    # клавиатура для начального выбора
-    keyboard = ReplyKeyboardMarkup()
-    button1 = KeyboardButton('запись через салон')
-    button2 = KeyboardButton('запись через мастера')
-    button3 = KeyboardButton('просто дайте мне номер телефона')
-    keyboard.add(button1, button2, button3)
 
     if message.text == "да":
         user_states[message.chat.id] = 'main_state'
-        bot.reply_to(message, 'Соглашение одобрено, как хочешь записаться?',
-                     reply_markup=keyboard)
+        services = Services.objects.all()
+        keyboard = ReplyKeyboardMarkup(resize_keyboard=True)  # уменьшены кнопки
+        for service in services:
+            keyboard.add(KeyboardButton(service.name))
+        bot.reply_to(message, 'Выберите услугу:', reply_markup=keyboard)
 
-    # если нет, то клавиатура удаляется, ничего не доступно
     elif message.text == "нет":
         user_states[message.chat.id] = 'declined'
-        bot.reply_to(message, 'Вы отклонили соглашение',
-                     reply_markup=remove_keyboard)
+        bot.reply_to(message, 'Вы отклонили соглашение', reply_markup=remove_keyboard)
 
+@bot.message_handler(func=lambda message: user_states.get(message.chat.id) == 'main_state')
+def handle_service_selection(message):
+    service_name = message.text
+    service = Services.objects.filter(name=service_name).first()
 
-@bot.message_handler(func=lambda message: user_states.get(
-        message.chat.id) == 'main_state')
-def handle_main_state(message):
-    # начальный этап
-
-    if message.text == "запись через салон":
-        # клавиатура салонов, надо соеденить с моделью салонов
-        keyboard1 = ReplyKeyboardMarkup()
-
-        button1 = KeyboardButton('салон 123')
-        button2 = KeyboardButton('салон 321')
-        button3 = KeyboardButton('салон 222')
-
-        keyboard1.add(button1, button2, button3)
-
-        user_states[message.chat.id] = 'salon'
-        bot.reply_to(message, 'выбери салон', reply_markup=keyboard1)
-
-    elif message.text == "запись через мастера":
-        # клавиатура процедур, тоже соеденить с бэком
+    if service:
+        user_states[message.chat.id] = 'select_specialist'
+        specialists = Specialist.objects.filter(service=service)
         inline_keyboard = InlineKeyboardMarkup()
+        for specialist in specialists:
+            inline_keyboard.add(InlineKeyboardButton(specialist.name, callback_data=specialist.name))
+        bot.send_message(message.chat.id, 'Выберите специалиста:', reply_markup=inline_keyboard)
+    else:
+        bot.reply_to(message, 'Такой услуги нет. Попробуйте снова.')
 
-        button1 = InlineKeyboardButton("процедура 1",
-                                       callback_data="procedure_1")
-        button2 = InlineKeyboardButton("процедура 2",
-                                       callback_data="procedure_2")
-        button3 = InlineKeyboardButton("процедура 3",
-                                       callback_data="procedure_3")
+@bot.callback_query_handler(func=lambda call: user_states.get(call.message.chat.id) == 'select_specialist')
+def handle_specialist_selection(call):
+    specialist_name = call.data
+    specialist = Specialist.objects.filter(name=specialist_name).first()
 
-        inline_keyboard.add(button1, button2, button3)
-
-        user_states[message.chat.id] = 'salon_master'
-        bot.reply_to(message, 'выбери процедуру', reply_markup=inline_keyboard)
-
-    elif message.text == "просто дайте мне номер телефона":
-        # просто возврат в начальный этап
-        keyboard = ReplyKeyboardMarkup()
-
-        button1 = KeyboardButton('запись через салон')
-        button2 = KeyboardButton('запись через мастера')
-        button3 = KeyboardButton('просто дайте мне номер телефона')
-
-        keyboard.add(button1, button2, button3)
-
-        user_states[message.chat.id] = 'main_state'
-        bot.reply_to(message, 'держи', reply_markup=keyboard)
-
-
-@bot.message_handler(func=lambda message: user_states.get(
-        message.chat.id) == 'salon')
-def handle_salon(message):
-    # выполняется, если мы выбрали 1 вариант: через салон, а затем выбрали салон
-
-    # логику выбора надо будет поменять
-    if message.text in ['салон 222', 'салон 321', 'салон 123']:
-        user_states[message.chat.id] = 'salon_date'
-        remove_keyboard = ReplyKeyboardRemove()
-
-        # та же клавиатура процедур, должна быть привязана к выбранному салону
+    if specialist:
+        user_states[call.message.chat.id] = 'select_time'
+        time_slots = TimeSlot.objects.filter(specialist=specialist, is_booked=False)
         inline_keyboard = InlineKeyboardMarkup()
+        for time_slot in time_slots:
+            inline_keyboard.add(InlineKeyboardButton(f'{time_slot.date} {time_slot.time}', callback_data=f'{time_slot.id}'))
+        bot.send_message(call.message.chat.id, 'Выберите время:', reply_markup=inline_keyboard)
+    else:
+        bot.send_message(call.message.chat.id, 'Такого мастера нет. Выберите другого мастера:')
+        # Повторить выбор мастера
+        service_name = user_states.get(call.message.chat.id, {}).get('service_name', '')
+        service = Services.objects.filter(name=service_name).first()
+        if service:
+            specialists = Specialist.objects.filter(service=service)
+            inline_keyboard = InlineKeyboardMarkup()
+            for specialist in specialists:
+                inline_keyboard.add(InlineKeyboardButton(specialist.name, callback_data=specialist.name))
+            bot.send_message(call.message.chat.id, 'Выберите специалиста:', reply_markup=inline_keyboard)
 
-        button1 = InlineKeyboardButton("процедура 1",
-                                       callback_data="procedure_1")
-        button2 = InlineKeyboardButton("процедура 2",
-                                       callback_data="procedure_2")
-        button3 = InlineKeyboardButton("процедура 3",
-                                       callback_data="procedure_3")
-        inline_keyboard.add(button1, button2, button3)
+@bot.callback_query_handler(func=lambda call: user_states.get(call.message.chat.id) == 'select_time')
+def handle_time_slot_selection(call):
+    time_slot_id = call.data
+    time_slot = TimeSlot.objects.filter(id=time_slot_id).first()
 
-        bot.send_message(message.chat.id, 'выбери процедуру',
-                         reply_markup=remove_keyboard)
-        bot.send_message(message.chat.id, 'процедуры',
-                         reply_markup=inline_keyboard)
+    if time_slot:
+        user_states[call.message.chat.id] = 'enter_phone'
+        bot.send_message(call.message.chat.id, 'Введите ваш номер телефона:') 
 
+        # Сохраняем информацию о временном слоте
+        user_states[call.message.chat.id] = {
+            'time_slot': time_slot,
+            'specialist': time_slot.specialist,
+            'service': time_slot.specialist.service.first()
+        }
+    else:
+        bot.send_message(call.message.chat.id, 'Такого времени нет. Выберите другое время:')
+        # Предложить выбрать время заново
+        specialist_name = user_states.get(call.message.chat.id, {}).get('specialist_name', '')
+        specialist = Specialist.objects.filter(name=specialist_name).first()
+        if specialist:
+            time_slots = TimeSlot.objects.filter(specialist=specialist, is_booked=False)
+            inline_keyboard = InlineKeyboardMarkup()
+            for time_slot in time_slots:
+                inline_keyboard.add(InlineKeyboardButton(f'{time_slot.date} {time_slot.time}', callback_data=f'{time_slot.id}'))
+            bot.send_message(call.message.chat.id, 'Выберите время:', reply_markup=inline_keyboard)
 
-@bot.callback_query_handler(func=lambda call: user_states.get(
-        call.message.chat.id) == 'salon_date')
-def handle_salon_date(call):
-    # выполняется после выбора процедуры в варианте 1
-    # логику выбора процедур надо будет так же поменять
-
-    if call.data in ["procedure_1", 'procedure_2', 'procedure_3']:
-        inline_keyboard = InlineKeyboardMarkup()
-        button1 = InlineKeyboardButton("01.01",
-                                       callback_data="date_1")
-        button2 = InlineKeyboardButton("02.01",
-                                       callback_data="date_2")
-        button3 = InlineKeyboardButton("03.01",
-                                       callback_data="date_3")
-        inline_keyboard.add(button1, button2, button3)
-        bot.send_message(call.message.chat.id, "процедура выбрана \n"
-                         'выбери дату',
-                         reply_markup=inline_keyboard)
-        user_states[call.message.chat.id] = 'salon_time'
-
-
-@bot.callback_query_handler(func=lambda call: user_states.get(
-        call.message.chat.id) == 'salon_time')
-def handle_salon_time(call):
-    # выполняется после выбора даты
-
-    if call.data in ['date_1', 'date_2', 'date_3']:
-
-        # клавиатура выбора времени должна создаваться с помощью моделей
-
-        inline_keyboard = InlineKeyboardMarkup()
-
-        button1 = InlineKeyboardButton("14:00",
-                                       callback_data="time_1")
-        button2 = InlineKeyboardButton("13:00",
-                                       callback_data="time_2")
-        button3 = InlineKeyboardButton("12:00",
-                                       callback_data="time_3")
-        inline_keyboard.add(button1, button2, button3)
-
-        user_states[call.message.chat.id] = 'salon_phone'
-        bot.send_message(call.message.chat.id, 'дата выбрана, выбери время',
-                         reply_markup=inline_keyboard)
-
-
-@bot.callback_query_handler(func=lambda call: user_states.get(
-        call.message.chat.id) == 'salon_phone')
-def handle_salon_phone(call):
-
-    # выполняется после выбора времени
-
-    if call.data in ['time_1', 'time_2', 'time_3']:
-        bot.send_message(call.message.chat.id, 'Напишите номер телефона')
-        user_states[call.message.chat.id] = 'salon_success'
-
-
-@bot.message_handler(func=lambda message: user_states.get(
-        message.chat.id) == 'salon_success')
+@bot.message_handler(func=lambda message: user_states.get(message.chat.id) == 'enter_phone')
 def handle_phone_number(message):
-    # клавиатура начального экрана
-    keyboard = ReplyKeyboardMarkup()
+    phone_number = message.text
+    user_data = user_states[message.chat.id]
+    time_slot = user_data.get('time_slot')
+    specialist = user_data.get('specialist')
+    service = user_data.get('service')
 
+    if not time_slot or not specialist or not service:
+        bot.send_message(message.chat.id, 'Ошибка! Пожалуйста, попробуйте заново.')
+        return
+
+    client = Clients(name=message.from_user.first_name,
+                     telegram_username=message.from_user.username,
+                     phone_number=phone_number)
+    client.save()
+
+    # Бронирование
+    schedule = Schedule(client=client,
+                        specialist=specialist,
+                        service=service,
+                        time_slot=time_slot)
+    schedule.save()
+
+    # Обновляем временной слот как занятый
+    time_slot.is_booked = True
+    time_slot.save()
+
+    bot.send_message(message.chat.id, f'Ваше бронирование на услугу "{service.name}" с мастером {specialist.name} на {time_slot.date} в {time_slot.time} подтверждено!')
+
+    # Завершаем процесс
+    user_states[message.chat.id] = 'main_state'
+    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)  # уменьшены кнопки
     button1 = KeyboardButton('запись через салон')
     button2 = KeyboardButton('запись через мастера')
     button3 = KeyboardButton('просто дайте мне номер телефона')
-
     keyboard.add(button1, button2, button3)
-
-    phone_number = message.text
-    # phone_number, как я понял, надо добваить в бд
-    # и не только phone_number нужно сохранять все данные в бд
-    # должна быть модель записи
-    user_states[message.chat.id] = 'main_state'
-    bot.send_message(message.chat.id, 'Номер записан, возврат к началу',
-                     reply_markup=keyboard)
-
-
-@bot.callback_query_handler(func=lambda call: user_states.get(
-        call.message.chat.id) == 'salon_master')
-def handle_master(call):
-    # Выполняется после выбора процедуры в варианте 2
-    if call.data in ["procedure_1", 'procedure_2', 'procedure_3']:
-        # клавиатура мастеров на эти процедуры, надо соеденять с бэком
-        inline_keyboard = InlineKeyboardMarkup()
-
-        button1 = InlineKeyboardButton("Адольф",
-                                       callback_data="master_1")
-        button2 = InlineKeyboardButton("Никита",
-                                       callback_data="master_2")
-        button3 = InlineKeyboardButton("Марина",
-                                       callback_data="master_3")
-        inline_keyboard.add(button1, button2, button3)
-
-        bot.send_message(call.message.chat.id, 'выбери мастера',
-                         reply_markup=inline_keyboard)
-        user_states[call.message.chat.id] = 'master_date'
-
-
-@bot.callback_query_handler(func=lambda call: user_states.get(
-        call.message.chat.id) == 'master_date')
-def handle_master_date(call):
-    # выполняется после выбора мастера
-    if call.data in ["master_1", 'master_2', 'master_3']:
-        # клавиатура выбора дат, должно быть связано с бэком
-        inline_keyboard = InlineKeyboardMarkup()
-
-        button1 = InlineKeyboardButton("01.01",
-                                       callback_data="date_1")
-        button2 = InlineKeyboardButton("02.01",
-                                       callback_data="date_2")
-        button3 = InlineKeyboardButton("03.01",
-                                       callback_data="date_3")
-        inline_keyboard.add(button1, button2, button3)
-
-        bot.send_message(call.message.chat.id, 'выбери дату',
-                         reply_markup=inline_keyboard)
-        user_states[call.message.chat.id] = 'master_time'
-
-
-@bot.callback_query_handler(func=lambda call: user_states.get(
-        call.message.chat.id) == 'master_time')
-def handle_master_time(call):
-    # выполняется после выбора даты
-    if call.data in ["date_1", 'date_2', 'date_3']:
-        # клавиатура выбора времени, должно быть связано с бэком
-        inline_keyboard = InlineKeyboardMarkup()
-
-        button1 = InlineKeyboardButton("12:00",
-                                       callback_data="time_1")
-        button2 = InlineKeyboardButton("13:00",
-                                       callback_data="time_2")
-        button3 = InlineKeyboardButton("14:00",
-                                       callback_data="time_3")
-        inline_keyboard.add(button1, button2, button3)
-        bot.send_message(call.message.chat.id, 'выбери время',
-                         reply_markup=inline_keyboard)
-        user_states[call.message.chat.id] = 'master_success'
-
-
-@bot.callback_query_handler(func=lambda call: user_states.get(
-        call.message.chat.id) == 'master_success')
-def handle_master_success(call):
-    # выполняется после выбора времени
-    if call.data in ["time_1", 'time_2', 'time_3']:
-        # просто начальная клавиатура
-        keyboard = ReplyKeyboardMarkup()
-        button1 = KeyboardButton('запись через салон')
-        button2 = KeyboardButton('запись через мастера')
-        button3 = KeyboardButton('просто дайте мне номер телефона')
-        keyboard.add(button1, button2, button3)
-
-        bot.send_message(call.message.chat.id, 'вы записаны, ваш салон',
-                         reply_markup=keyboard)
-        user_states[call.message.chat.id] = 'main_state'
-
+    bot.send_message(message.chat.id, 'Запись завершена, возврат в меню', reply_markup=keyboard)
 
 if __name__ == "__main__":
     bot.polling()
-
-
-# это просто интерфейс и логика в нём немного глупая
-# нужно будет доделать клавиатуры, чтобы они отображали актуальные данные
-# нужно будет сделать нормальное сохранение данных в бд
